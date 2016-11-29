b4dfbba4aaf267a60e3884ff52dd7d7a7920d78b
Brandon <brandon@users.noreply.github.com>
2016-11-29 10:59:33 -0800

    idk

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,7 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
-function left() {
+function leftl() {
 var myCircle = document.createElementNS(namespace, "circle")
 myCircle.setAttribute("cx",50)
 myCircle.setAttribute("cy",50)
